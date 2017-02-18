21389f568f49c48a0ffaa76f33b5f1556858a9e9
“sawaf99” <b00051562@aus.edu>
2017-02-18 15:58:25 +0400

    Feature B added

--- src/la3scm/LA3SCM.java
+++ src/la3scm/LA3SCM.java
@@ -14,6 +14,9 @@ public class LA3SCM {
     /**
      * @param args the command line arguments
      */
+    
+    public void addFeaturB(){}
+            
     public static void main(String[] args) {
         
         System.out.println("Hello World");
